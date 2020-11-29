06238b4455208421fc7aad393d618fc9993fad7d
Rany Elhousieny <elhousie@gmail.com>
2020-11-29 10:08:02 -0800

    UserMicroservice

---
+++ UserMicroservice/src/test/java/com/elhousieny/UserMicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elhousieny.UserMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
